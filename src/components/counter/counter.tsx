3bc65fe150968284cafadcca1686aa13a4cd12f6
Nadine <nadlgit@users.noreply.github.com>
2022-09-05 22:23:38 -0400

    feat: countdown

--- src/components/counter/counter.tsx
+++ src/components/counter/counter.tsx
@@ -1,11 +1,36 @@
 import styles from './counter.module.css';
 import { CounterItem } from './counter-item';
+import { getLaunchDate, getRemainingTimeFromDate } from '../../helpers';
+import { useEffect, useState } from 'react';
 
-export const Counter = () => (
-  <div className={styles.counter}>
-    <CounterItem label="days" currentValue={8} />
-    <CounterItem label="hours" currentValue={23} />
-    <CounterItem label="minutes" currentValue={55} />
-    <CounterItem label="seconds" currentValue={41} />
-  </div>
-);
+import type { RemainingTime } from '../../helpers';
+
+export const Counter = () => {
+  const [launchDate, setLaunchDate] = useState<Date | null>();
+  const [remaining, setRemaining] = useState<RemainingTime>({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const launch = getLaunchDate();
+    setLaunchDate(launch);
+    setRemaining(getRemainingTimeFromDate(launch));
+
+    const intervalId = setInterval(() => {
+      setRemaining(getRemainingTimeFromDate(launchDate ?? launch));
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className={styles.counter}>
+      <CounterItem label="days" currentValue={remaining.days} />
+      <CounterItem label="hours" currentValue={remaining.hours} />
+      <CounterItem label="minutes" currentValue={remaining.minutes} />
+      <CounterItem label="seconds" currentValue={remaining.seconds} />
+    </div>
+  );
+};
---
+++ src/helpers/helpers.ts
@@ -0,0 +1,35 @@
+export function getLaunchDate() {
+  const result = new Date();
+  result.setDate(result.getDate() + 14);
+  result.setHours(9);
+  result.setMinutes(30);
+  result.setSeconds(0);
+  result.setMilliseconds(0);
+  return result;
+}
+
+export type RemainingTime = {
+  days: number;
+  hours: number;
+  minutes: number;
+  seconds: number;
+};
+
+export function getRemainingTimeFromDate(date: Date): RemainingTime {
+  const remainingTime = date ? date.getTime() - Date.now() : 0;
+  const result =
+    remainingTime > 0
+      ? {
+          days: Math.floor(remainingTime / 1000 / 60 / 60 / 24),
+          hours: Math.floor(remainingTime / 1000 / 60 / 60) % 24,
+          minutes: Math.floor(remainingTime / 1000 / 60) % 60,
+          seconds: Math.floor(remainingTime / 1000) % 60,
+        }
+      : {
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0,
+        };
+  return result;
+}
